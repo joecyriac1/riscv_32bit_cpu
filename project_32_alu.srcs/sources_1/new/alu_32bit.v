c5b177ab876af7e24c8cda47d002920c8b295998
joecyriac1 <joecyriac20@gmail.com>
2026-06-19 23:00:41 +0530

    Initial-32 bit alu project files

---
+++ project_32_alu.srcs/sources_1/new/alu_32bit.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+
+module alu_32bit(
+input clk,
+input reset,
+input [31:0]A,
+input [31:0]B,
+input [4:0]ALUControl,
+output[31:0]ALUResult,
+output reg Zero
+ );
+ 
+ 
+endmodule
